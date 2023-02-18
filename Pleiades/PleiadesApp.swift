4be2a6f8ddf4354ee0c719aa1d49025250e9b1a6
Scott Odle <scott@sjodle.com>
2023-02-18 07:08:09 -0700

    Initial Commit

---
+++ Pleiades/PleiadesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleiadesApp.swift
+//  Pleiades
+//
+//  Created by Scott Odle on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PleiadesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
